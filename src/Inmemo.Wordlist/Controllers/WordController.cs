be9bbc21683f8832015e73477a8261ed489ae641
Dmitry Gorshkov <dmitry.gorshkov@futurice.com>
2016-07-16 21:23:30 +0300

    Implement all controller unit tests

--- src/Inmemo.Wordlist/Controllers/WordController.cs
+++ src/Inmemo.Wordlist/Controllers/WordController.cs
@@ -28,16 +28,24 @@ namespace Inmemo.Wordlist.Controllers
         }
 
         [Route("{rank:int}")]
-        public async Task<JsonResult> Rank(int rank)
+        public async Task<IActionResult> Rank(int rank)
         {
             var word = await _repository.GetByRankAsync(rank);
+            if (word == null)
+            {
+                return NotFound();
+            }
             return Json(_mapper.Map<WordViewModel>(word));
         }
 
         [Route("{id:int}")]
-        public async Task<JsonResult> Id(int id)
+        public async Task<IActionResult> Id(int id)
         {
             var word = await _repository.GetByIdAsync(id);
+            if (word == null)
+            {
+                return NotFound();
+            }
             return Json(_mapper.Map<WordViewModel>(word));
         }
     }
--- test/Inmemo.Wordlist.Tests/Controllers/WordControllerTest.cs
+++ test/Inmemo.Wordlist.Tests/Controllers/WordControllerTest.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Inmemo.Wordlist.Controllers;
 using Inmemo.Wordlist.Models;
 using Inmemo.Wordlist.ViewModels;
 using Inmemo.Wordlist.Services;
@@ -10,53 +9,69 @@ using Moq;
 using Xunit;
 using AutoMapper;
 
-namespace Inmemo.Wordlist.Controllers.UnitTests
+namespace Inmemo.Wordlist.Controllers
 {
     public class WordControllerTest
     {
-        private List<Word> GetWords()
+        private Mock<IMapper> _mockMapper;
+        private Mock<IWordRepository> _mockWordRepository;
+
+        private Word GetWord()
         {
-            return new List<Word>{
-                new Word {
-                    Id = 647,
-                    Name = "time",
-                    Rank = 52,
-                    PartOfSpeech = PartOfSpeech.Noun,
-                    Spoken = 196659,
-                    Fiction = 172054,
-                    Magazine = 178141,
-                    Newspaper = 156965,
-                    Academic = 129155,
-                    Total = 832974
-                }
+            return new Word
+            {
+                Id = 647,
+                Name = "time",
+                Rank = 52,
+                PartOfSpeech = PartOfSpeech.Noun,
+                Spoken = 196659,
+                Fiction = 172054,
+                Magazine = 178141,
+                Newspaper = 156965,
+                Academic = 129155,
+                Total = 832974
             };
         }
 
-        private List<WordViewModel> GetWordViewModelList()
+        private List<Word> GetWordList()
         {
-            return new List<WordViewModel>{
-                new WordViewModel{
-                    Id = 647,
-                    Name = "time",
-                    Rank = 52
-                }
+            return new List<Word>{
+                GetWord()
             };
+        }
+
+        private List<Word> GetEmptyWordList()
+        {
+            return new List<Word>();
+        }
+
+        private WordViewModel GetWordViewModel(Word word){
+            return new WordViewModel { Id = word.Id, Name = word.Name, Rank = word.Rank };
+        }
+
+        private List<WordViewModel> GetWordViewModelList(List<Word> words)
+        {
+            return words.Select(w => new WordViewModel { Id = w.Id, Name = w.Name, Rank = w.Rank }).ToList();
+        }
+
+        public WordControllerTest()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _mockMapper.Setup(mapper => mapper.Map<WordViewModel>(It.IsAny<Word>())).Returns((Word w) => GetWordViewModel(w));
+            _mockMapper.Setup(mapper => mapper.Map<List<WordViewModel>>(It.IsAny<List<Word>>())).Returns((List<Word> wl) => GetWordViewModelList(wl));
+            _mockWordRepository = new Mock<IWordRepository>();
         }
 
         [Fact]
-        public async Task Name_ReturnsWordList()
+        public async Task NameShouldReturnsWordList()
         {
             // Arrange
-            var testName = "time";
-            var wordList = GetWords();
-            var mockRepo = new Mock<IWordRepository>();
-            mockRepo.Setup(repo => repo.GetByNameAsync(testName)).Returns(Task.FromResult(wordList));
-            var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(mapper => mapper.Map<List<WordViewModel>>(wordList)).Returns(GetWordViewModelList());
-            var controller = new WordController(mockRepo.Object, mockMapper.Object);
+            var testWordName = "time";
+            _mockWordRepository.Setup(repo => repo.GetByNameAsync(testWordName)).Returns(Task.FromResult(GetWordList()));
+            var controller = new WordController(_mockWordRepository.Object, _mockMapper.Object);
 
             // Act
-            var result = await controller.Name(testName);
+            var result = await controller.Name(testWordName);
 
             // Assert
             var jsonResult = Assert.IsType<JsonResult>(result);
@@ -65,6 +80,91 @@ namespace Inmemo.Wordlist.Controllers.UnitTests
             Assert.Equal(647, word.Id);
             Assert.Equal("time", word.Name);
             Assert.Equal(52, word.Rank);
+        }
+
+        [Fact]
+        public async Task NameShouldReturnsEmptyListWhenRepositoryReturnsEmptyList()
+        {
+            // Arrange
+            var testWordName = "nonexistent_word";
+            _mockWordRepository.Setup(repo => repo.GetByNameAsync(testWordName)).Returns(Task.FromResult(GetEmptyWordList()));
+            var controller = new WordController(_mockWordRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await controller.Name(testWordName);
+
+            // Assert
+            var jsonResult = Assert.IsType<JsonResult>(result);
+            var returnValue = Assert.IsType<List<WordViewModel>>(jsonResult.Value);
+            Assert.Equal(0, returnValue.Count());
+        }
+
+        [Fact]
+        public async Task IdShouldReturnsWord()
+        {
+            // Arrange
+            var testWordId = 647;
+            _mockWordRepository.Setup(repo => repo.GetByIdAsync(testWordId)).Returns(Task.FromResult(GetWord()));
+            var controller = new WordController(_mockWordRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await controller.Id(testWordId);
+
+            // Assert
+            var jsonResult = Assert.IsType<JsonResult>(result);
+            var word = Assert.IsType<WordViewModel>(jsonResult.Value);
+            Assert.Equal(647, word.Id);
+            Assert.Equal("time", word.Name);
+            Assert.Equal(52, word.Rank);
+        }
+
+        [Fact]
+        public async Task IdShouldReturnsNotFoundWhenRepositoryReturnsNull()
+        {
+            // Arrange
+            var testWordId = 100500;
+            _mockWordRepository.Setup(repo => repo.GetByIdAsync(testWordId)).Returns(Task.FromResult((Word)null));
+            var controller = new WordController(_mockWordRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await controller.Id(testWordId);
+
+            // Assert
+            var jsonResult = Assert.IsType<NotFoundResult>(result);
+        }
+
+                [Fact]
+        public async Task RankShouldReturnsWord()
+        {
+            // Arrange
+            var testWordRank = 52;
+            _mockWordRepository.Setup(repo => repo.GetByRankAsync(testWordRank)).Returns(Task.FromResult(GetWord()));
+            var controller = new WordController(_mockWordRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await controller.Rank(testWordRank);
+
+            // Assert
+            var jsonResult = Assert.IsType<JsonResult>(result);
+            var word = Assert.IsType<WordViewModel>(jsonResult.Value);
+            Assert.Equal(647, word.Id);
+            Assert.Equal("time", word.Name);
+            Assert.Equal(52, word.Rank);
+        }
+
+        [Fact]
+        public async Task RankShouldReturnsNotFoundWhenRepositoryReturnsNull()
+        {
+            // Arrange
+            var testWordRank = 100500;
+            _mockWordRepository.Setup(repo => repo.GetByRankAsync(testWordRank)).Returns(Task.FromResult((Word)null));
+            var controller = new WordController(_mockWordRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await controller.Id(testWordRank);
+
+            // Assert
+            var jsonResult = Assert.IsType<NotFoundResult>(result);
         }
     }
 }
